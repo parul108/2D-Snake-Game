f991c08660c6987e8272aa7bf0f1edce05ad07a3
Parul Mudaliar <73463780+parul108@users.noreply.github.com>
2021-07-01 16:44:56 +0530

    game

---
+++ game.cpp
@@ -0,0 +1,91 @@
+#include "game.h"
+
+// int x = 10 , y = 10;
+int game_state = BEFORE_START; // by default
+
+int direction;
+
+int score;
+
+void start_game(){
+    init_snake();
+    init_food();
+    direction = UP; // intially 
+    game_state = STARTED;
+    score = 0;
+}
+
+void end_game(){
+    // end state -> before_start state
+    game_state = ENDED;
+}
+
+void paint_status(){
+    move(0 , 5);
+    // printf
+    printw("Score %d" , score);
+}
+
+bool game_logic(){
+    paint_border();
+    paint_status();
+
+    int key = getch(); // blocking input
+
+    if(game_state == BEFORE_START){
+        move(10 , 10);
+        addstr("Press space to start");
+        if(key == 32){
+            // start the game
+            start_game();
+        }
+    } else if(game_state == STARTED) {
+        // either keep moving snake
+        // or react to key inputs
+        if(key == UP && direction != DOWN){
+            direction = UP;
+        } else if (key == DOWN && direction != UP){
+            direction = DOWN;
+        } if(key == LEFT && direction != RIGHT){
+            direction = LEFT;
+        } else if (key == RIGHT && direction != LEFT){
+            direction = RIGHT;
+        }
+        pair < int , int > head = move_snake(direction);
+        // foood position == head of snake
+        // if its true , then eat the food and grow the snake
+        if(try_eating_food(head)){
+            score ++;
+            grow_snake();
+        }
+        if(has_collision()){
+            // end the game
+            end_game();
+        }
+        paint_snake();
+        paint_food();
+    } else {
+        // state is ended
+        move(10, 10);
+        addstr("GAME OVER! Press space to restart , q to quit");
+        if(key == 32){
+            start_game();
+        } else if(key == 'q'){
+            return true; // game_over
+        }
+    }
+    return false;
+
+    // if(key == UP){
+    //     x--;
+    // } else if (key == DOWN){
+    //     x++;
+    // } if(key == LEFT){
+    //     y--;
+    // } else if (key == RIGHT){
+    //     y++;
+    // }
+    // move(x , y);
+    // addch('#');
+    
+}
